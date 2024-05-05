cb8d399ce0bc13a91d8d2cc11b8cd4304cf13c94
Phap Huynh Van <huynhvanphap198@gmail.com>
2024-05-05 16:41:36 +0700

    config port for local

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 3000, // Thay đổi cổng mặc định 8080 thành 3000
+    open: true, // Tự động mở trình duyệt khi ứng dụng khởi chạy
+    proxy: {
+      // Cấu hình proxy nếu cần thiết
+    },
+  },
 });
