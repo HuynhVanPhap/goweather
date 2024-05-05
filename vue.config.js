6c9a00c05177efdd01623b55a98e1d6ed3087373
Phap Huynh Van <huynhvanphap198@gmail.com>
2024-05-05 16:43:23 +0700

    config port for local update

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3000, // Thay đổi cổng mặc định 8080 thành 3000
+    // host: "localhost", // Tên máy chủ mặc định là 'localhost'
     open: true, // Tự động mở trình duyệt khi ứng dụng khởi chạy
     proxy: {
       // Cấu hình proxy nếu cần thiết
